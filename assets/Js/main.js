5830c1a5f80c8dbee2eb319dda282ee49e81c9c8
Julien loriot <jloriot@normandiewebschool.fr>
2021-05-27 11:53:25 +0200

    recette

---
+++ assets/Js/main.js
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charsert="UTF-8">
+        <meta name="viewport" content=" width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" media="screen" href="assets/css/bootstrap.css?v=1" />
+        <link rel="stylesheet" type="text/css" media="screen" href="assets/css/styles.css?v=1" />
+        <script src="assets/js/main.js"></script>
+        <title>Julien Loriot</title>
+        <style>
+            body.grey {
+                background: grey;
+            }
+            body.black {
+                background: black;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+
+            <h1>Julien Loriot</h1>
+            <h2>Étudiant à la Normandie Web School<br>Chef de projet digital</h2>
+            <p></p>
+            <img src="Unknown.png"/>
+        </header>
+        <img src="assets/image/abstract-69124.jpg">
+
+        <section id="contact">
+            <p class="address">18 route de la ferme des portes<br/>27500 Fourmetot </p>
+            <p class="tel">
+                <a href="tel:0770309937">07 70 30 99 37</a>
+            </p>
+            <p class="email">
+                <a href="mailot:jloriot@normandiewebschool.fr">jloriot@normandiewebschool.fr</a>
+            </p>
+        </section>
+
+        <section id="pro">
+            <h1>Expériences professionnelles</h1>
+            <ul class="years">
+                <li class="year">
+                    <h3>1997</h3>
+                    <ul class="experience">
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                    </ul>
+                </li>
+                <li class="year">
+                    <h3>1997</h3>
+                    <ul class="experience">
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                    </ul>
+                </li>
+                <li class="year">
+                    <h3>1997</h3>
+                    <ul class="experience">
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+
+        <section id="school">
+            <h1>Formations</h1>
+            <ul class="year">
+                <li class="year">
+                    <h3>1997</h3>
+                    <ul class="experience">
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                    </ul>
+                </li>
+                <li class="year">
+                    <h3>1997</h3>
+                    <ul class="experience">
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                    </ul>
+                </li>
+                <li class="year">
+                    <h3>1997</h3>
+                    <ul class="experience">
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                        <li class="experience">
+                            <h4>Site web ENSAD</h4>
+                            <p class="description"> Lorem ipsum Lorem ipsum Lorem ipsum
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+
+        <section id="logos">
+            
+            <ul>
+                <li class="logos">
+                    Suite Adobe
+                </li>
+                <li class="logos">
+                    Suite Adobe
+                </li>
+                <li class="logos">
+                    Suite Adobe
+                </li>
+                <li class="logos">
+                    Suite Adobe
+                </li>
+            </ul>
+        </section>
+    </body>
+</html>
